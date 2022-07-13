04a5a51c4ecd7b9b6eff1ae362cbeca9b1c4aa99
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2022-07-13 11:25:10 +0530

    Digit count is modified

--- src/ConditionsAndLoops/DigitCount.java
+++ src/ConditionsAndLoops/DigitCount.java
@@ -7,7 +7,7 @@ public class DigitCount{
     }
     static void counter(int n){
         int count = 0;
-        while(n>0){
+        while(n!=0){
             int rem = n%10;
             if(rem<=10){
                 count+=1;
