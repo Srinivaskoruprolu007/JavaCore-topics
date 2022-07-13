3f38352ddc4032ba18f4b47a3d22b4163143fb59
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2022-07-13 11:02:47 +0530

    MaximumIn2D.java is updated

--- src/Searching/MaximumIn2D.java
+++ src/Searching/MaximumIn2D.java
@@ -1,5 +1,24 @@
 package Searching;
 
 public class MaximumIn2D {
+    public static void main(String[] args) {
+        int[][] numbers = {
+                {2,3,4},
+                {5,6,7,8},
+                {9,10,11}
+        };
+        System.out.println(max(numbers));
 
+    }
+    static int max(int[][]arr){
+        int max = Integer.MIN_VALUE;
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                if (anInt > max) {
+                    max = anInt;
+                }
+            }
+        }
+        return max;
+    }
 }
