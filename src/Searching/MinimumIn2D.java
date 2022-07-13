394284789c984b15a3d706559bd9c3f6768889dd
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2022-07-13 11:19:17 +0530

    MinimumIn2D.java is added

---
+++ src/Searching/MinimumIn2D.java
@@ -0,0 +1,23 @@
+package Searching;
+
+public class MinimumIn2D {
+    public static void main(String[] args) {
+        int[][] numbers = {
+                {-1,3,4},
+                {5,6,7,8},
+                {9,10,11}
+        };
+        System.out.println(min(numbers));
+    }
+    static int min(int[][] arr){
+        int min = Integer.MAX_VALUE;
+        for (int[] num:arr) {
+            for (int n:num) {
+                if (n<min){
+                    min = n;
+                }
+            }
+        }
+        return min;
+    }
+}
